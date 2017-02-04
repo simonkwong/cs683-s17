614d06d2587e05d5d61fb1e4b3119d81a42e09fd
simonkwong <simonkwong92@gmail.com>
2017-02-03 17:49:14 -0800

    Removing Passwords

--- lab2/config/__init__.py
+++ lab2/config/__init__.py
@@ -1,13 +1,7 @@
-# DB_INFO = {'host':'cs683-db.cektvd31q8sm.us-west-2.rds.amazonaws.com',
-#            'port':3306,
-#            'user':'simonkwong',
-#            'password':'awsNcc17o1ee1o71ccn!',
-#            'database':'cs683_db'}
-
 DB_INFO = {'host':'127.0.0.1',
            'port':3306,
            'user':'root',
-           'password':'ncc17o1ee1o71ccn',
+           'password':'',
            'database':'cs683_db'}
 
 HOST = '0.0.0.0'
