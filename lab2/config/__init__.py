db76db58eba5909a0cdb1077a2196a3be6fcd8fc
simonkwong <simonkwong92@gmail.com>
2017-02-15 11:40:51 -0800

    Caching Gitignore

--- lab2/config/__init__.py
+++
@@ -1,8 +0,0 @@
-DB_INFO = {'host':'192.168.136.1',
-           'port':3306,
-           'user':'simon',
-           'password':'ncc17o1e',
-           'database':'cs683_db'}
-
-HOST = '0.0.0.0'
-PORT = '8080'
--- lab3/config/__init__.py
+++
@@ -1,4 +0,0 @@
-HOST = '0.0.0.0'
-PORT = '8080'
-PCAP = 'resources/cookies_no_ssl.pcap'
-HTML = 'resources/stolen_html.html'
